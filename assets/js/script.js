d4ff3156466ad510ea657f1139b51bc55989e7a4
Elena Carey <theelenacarey@gmail.com>
2022-06-08 11:00:02 -0500

    Added save to local storage and retrieval from local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 //declare html variables
-var search = document.querySelector("#search");
-var citySearchEl = document.querySelector("#user-form");
-var todayContainer = document.querySelector("#today-container");
-var forecastContainer = document.querySelector("#forecast-container");
-var cityBox = document.querySelector("#city-box");
-
-
+var search = document.getElementById("search");
+var citySearchEl = document.getElementById("user-form");
+var todayContainer = document.getElementById("today-container");
+var forecastContainer = document.getElementById("forecast-container");
+var cityBox = document.getElementById("city-box");
+var passCity = document.getElementsByClassName("searchCity");
+var colorful = document.getElementsByClassName("colorful-uvi");
 //save array
 var searchedCities = JSON.parse(localStorage.getItem("searchedCities")) || [];
 
@@ -14,17 +14,16 @@ var searchedCities = JSON.parse(localStorage.getItem("searchedCities")) || [];
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
-    var cityName = search.value.trim();
+    var cityName = search.value.trim().toLowerCase();
     
     if (cityName) {
         getWeatherInfo(cityName);
-        //saveCities(cityName, infoLink);
-        //search.value = "";
-        console.log(cityName);
+        search.value = "";
     } else {
         redAlert();
     }
 };
+
 // warning function
 var redAlert = function(){
     var undo = function(){
@@ -49,50 +48,57 @@ var getWeatherInfo = function(city) {
         showWeather(data);
       });
     } else {
-      alert('Error: City Not Found');
+      search.textarea = "Error: City Not found.";
     }
   })
   .catch(function(error) {
     // Notice this `.catch()` getting chained onto the end of the `.then()` method
-    alert("Unable to connect to GitHub");
+    
   });
 };
 
-
-
-//pick out information from api
+//pick out information from api and call 2nd api
 var showWeather = function(response){
-  console.log(response.city.coord);
   var lon = response.city.coord.lon;
   var lat = response.city.coord.lat;
-
+  var city = response.city.name;
+  //save the city to the array
+  
+  saveCities(city);
+  displayCities();
+  //fetch weather api via coordinants
   var uviURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&lang=en&units=imperial&appid=42bd6e08d7e2ce3a0832c24d332bfd1e`
     fetch(uviURL)
     .then(function(resp) {
       if (resp.ok){
         resp.json().then(function(data) {
-          console.log(data);
+          //current weather information
+          //uvi colors
+          colorUv(data);
+          
+          
+
           var dt = new Date(data.current.dt * 1000);
           todayContainer.innerHTML = `<h4 class="card-title p-2">${response.city.name} (${dt.toDateString()}) <img 
         src="http://openweathermap.org/img/wn/${data.current.weather[0].icon}.png" alt="${data.current.weather[0].description}"/></h2>
         <p>Temp: ${data.current.temp}&deg;F</p>
         <p>Wind: ${data.current.wind_speed} MPH</p>
         <p>Humidity: ${data.current.humidity}%</p>
-        <p>UV Index: <span>${data.current.uvi}</span></p>`;
+        <p>UV Index: <span class="colorful-uvi">${data.current.uvi}</span></p>`;
+        //5 day forecast loop
 
         forecastContainer.innerHTML = data.daily
           .map((day, idx) => {
               if (idx > 0 && idx < 6) {
                 var dt = new Date(day.dt * 1000);
-                console.log(dt);
                 return ` 
                 <div class="card">
                     <p>(${dt.toDateString()})</p>
                     <p> <img 
                     src="http://openweathermap.org/img/wn/${day.weather[0].icon}.png" alt="${day.weather[0].description}"/></p>
-                    <p>Temp: ${day.temp.max}&deg;</p>
-                    <p>Wind: ${day.wind_speed}</p>
-                    <p>Humidity: ${day.humidity}</p>
+                    <p>Temp: ${day.temp.max}&deg;F</p>
+                    <p>Wind: ${day.wind_speed}mph</p>
+                    <p>Humidity: ${day.humidity}%</p>
                 </div>`
               }
         }).join('')
@@ -100,39 +106,63 @@ var showWeather = function(response){
     )}
   });
 }
-//display information about api
 
+//UVI Color Coding
+var colorUv = function(data){
+  var uvi = parseInt(data.current.uvi);
+  console.log(uvi)
+  // if(uvi <= 2){
+  //   colorful.classList.add = "green";
+  // } else if(uvi > 2 && uvi < 6) {
+  //   colorful.classList.add= "yellow";
+  // } else if(uvi > 5 && uvi < 8) {
+  //   colorful.classList.add= "orange";
+  // } else {
+  //   colorful.classList.add = "red";
+  // }
+};
 
 //save cities
-// var saveCities = function(cityName, infoLink) {
-//     searchedCities[cityName] = infoLink;
-//     localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
+var saveCities = function(cityName) {
+  if(searchedCities.includes(cityName)){
+      let index = searchedCities.indexOf(cityName);
+      searchedCities.splice(index,1);
+  } 
+    searchedCities.push(cityName);
+    localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
     
-// };
+};
 
-// //display cities
-// var displayCities = function(cities, searchTerm) {
-//     searchedCities = JSON.parse("searchedCities");
-//     var cityName = searchedCities[i];
+//Click Function handler for weather buttons
+var cityClickFunction = function(){
+  var chunk = this.textContent;
+  getWeatherInfo(chunk);
+};
+
+// //display cities function
+var displayCities = function() {
+    cityBox.innerHTML = "";
+    var lastTen = searchedCities.slice(-10).reverse();
+    for (var i = 0; i < lastTen.length; i++) {
+      var singleCity = lastTen[i];
+      
+        //create  container for each city searched
+        var cityEl = document.createElement("div");
+        cityEl.classList = "cityEl button list-item flex-row";
+        cityEl.innerHTML = `<p class="searchCity">${singleCity}</p>`;
+        cityBox.appendChild(cityEl);
+        cityEl.addEventListener("click", cityClickFunction);
+        cityEl.style.textTransform="capitalize";
+    }
     
-//     for (var i = 0; i < searchedCities.length; i++) {
-//         //create  container for each city searched
-//         var cityEl = document.createElement("a");
-//         cityEl.classList = "button list-item flex-row";
-//         cityEl.setAttribute("href","./html?info=" );
-//         cityEl.textContent = cityName;
-        
-//         cityBox.appendChild(cityEl);
-//     }
-// };
+    
+};
 
-//display forecast
+//display cities on page
+displayCities();
 
 
-//display 5 day forecast
+
 
 //event listeners
- citySearchEl.addEventListener("submit", formSubmitHandler);
-
-
-
+citySearchEl.addEventListener("submit", formSubmitHandler);
